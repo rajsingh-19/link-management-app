3f2944d1eeac9f5f71f54b56230dd74748e37539
Raj Singh <rajsingh.19.vns@gmail.com>
2025-01-22 17:45:50 +0530

    Developed the basic routing of login and register page and set the initial set-up

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
